33a0fc43994087f032e7562978f302a12ed53037
Michael Schatz <michael.schatz@live.de>
2017-08-06 00:31:09 +0200

    Add exception handler

---
+++ src/frontend/MatchMaker.UI/Services/Exception/ExceptionHandler.cs
@@ -0,0 +1,19 @@
+﻿using MatchMaker.Shared.Common;
+using MatchMaker.UI.Services.Alert;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MatchMaker.UI.Services.Exception
+{
+    public class ExceptionHandler : IExceptionHandler
+    {
+        public IAlertService AlertService => DependencyService.Get<IAlertService>();
+
+        public async Task Handle(System.Exception exception)
+        {
+            Guard.NotNull(exception, nameof(exception));
+
+            await this.AlertService.DisplayAlert("An error occured.", exception.Message);
+        }
+    }
+}
---
+++ src/frontend/MatchMaker.UI/Services/Exception/IExceptionHandler.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace MatchMaker.UI.Services.Exception
+{
+    public interface IExceptionHandler
+    {
+        Task Handle(System.Exception exception);
+    }
+}
