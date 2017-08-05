a2284e237e0af6160b8662bab2a09335ca93a1c8
Michael Schatz <michael.schatz@live.de>
2017-08-05 22:00:38 +0200

    Add guard check to services

--- src/frontend/MatchMaker.UI/Services/Authentication/AuthService.cs
+++ src/frontend/MatchMaker.UI/Services/Authentication/AuthService.cs
@@ -1,6 +1,6 @@
-﻿using System.Threading.Tasks;
-using MatchMaker.Shared.Accounts;
+﻿using MatchMaker.Shared.Common;
 using MatchMaker.UI.Services.ApiClient;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 [assembly: Dependency(typeof(MatchMaker.UI.Services.Authentication.AuthService))]
@@ -14,11 +14,17 @@ namespace MatchMaker.UI.Services.Authentication
 
         public async Task Register(string email, string password)
         {
+            Guard.NotNullOrWhiteSpace(email, nameof(email));
+            Guard.NotNullOrWhiteSpace(password, nameof(password));
+
             await this.ApiClient.Register(email, password);
         }
 
         public async Task Login(string email, string password)
         {
+            Guard.NotNullOrWhiteSpace(email, nameof(email));
+            Guard.NotNullOrWhiteSpace(password, nameof(password));
+
             this._token = await this.ApiClient.Login(email, password);
         }
     }
